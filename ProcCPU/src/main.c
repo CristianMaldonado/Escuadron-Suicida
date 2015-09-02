9d8ec5638a06115b954b2d0ce888b0ea89d38c14
fedehipper <federicohipper@gmail.com>
2015-09-02 19:31:12 -0300

    establecida la conexion entre Cpu y Planificador

--- ProcCPU/src/main.c
+++ ProcCPU/src/main.c
@@ -1,13 +1,8 @@
-/*
- * main.c
- *
- *  Created on: 2/9/2015
- *      Author: utnso
- */
 
 #include <pthread.h>
 #include "../../lib/libSocket.h"
-//#include "../lib/libSocket.c"
+
+
 
 void threadClient(){
 	int cli;
@@ -33,15 +28,33 @@ void threadServer(){
 
 int main(){
 
-	pthread_t tServer;
-	pthread_t tClient;
+	//pthread_t tServer;
+	//pthread_t tClient;
+
+	int serverSocket;
+	if(server_init(&serverSocket, "8888"))
+		printf("conexion establecida");
+
+	int socketCli;
+	if (client_init(&socketCli,"192.168.1.1", "8888"))
+		printf("no hubo error");
+
+	if(server_acept(serverSocket, &socketCli))
+		printf("acepada");
+
+	char package[10];
+
+	recv(socketCli, package, 10, 0);
+		printf("\n%s\n", package);
+
+
 
 	//creamos los hilos, ojo que no se verifica si falla. falla cuando devuelve distinto de cero
-	pthread_create(&tServer, NULL, (void*)&threadServer, NULL);
+	/*pthread_create(&tServer, NULL, (void*)&threadServer, NULL);
     pthread_create(&tClient, NULL, (void*)&threadClient, NULL);
 
     pthread_join(tServer, NULL);
     pthread_join(tClient, NULL);
-
+    */
 	return 0;
 }
