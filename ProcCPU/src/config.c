5b3bf0fe584b14b6b56baa62ef1f115d3da8c310
fedehipper <federicohipper@gmail.com>
2015-09-25 01:24:27 -0300

    quiete formato

--- ProcCPU/src/config.c
+++ ProcCPU/src/config.c
@@ -17,12 +17,10 @@ tipoConfiguracionCPU* leerConfiguracion() {
 	t_config* config;
 	config = config_create("../src/cpu.cfg");
 	datosCPU->cantidadHilos = config_get_int_value(config, "CANTIDAD_HILOS");
-	datosCPU->ipPlanificador = config_get_string_value(config,
-			"IP_PLANIFICADOR");
+	datosCPU->ipPlanificador = config_get_string_value(config, "IP_PLANIFICADOR");
 	datosCPU->ipMemoria = config_get_string_value(config, "IP_MEMORIA");
 	datosCPU->puertoMemoria = config_get_string_value(config, "PUERTO_MEMORIA");
-	datosCPU->puertoPlanificador = config_get_string_value(config,
-			"PUERTO_PLANIFICADOR");
+	datosCPU->puertoPlanificador = config_get_string_value(config, "PUERTO_PLANIFICADOR");
 	datosCPU->retardo = config_get_int_value(config, "RETARDO");
 	return datosCPU;
 }
