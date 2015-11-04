dde9c984bc287840bb053d753dca4ff95a4005c3
zevastian <sebastian.severino@hotmail.com.ar>
2015-11-04 16:26:52 -0300

    selector cambiado

--- ProcPlanificador/src/selector.c
+++ ProcPlanificador/src/selector.c
@@ -1,129 +1,164 @@
 #include "selector.h"
 #include "estructuras.h"
+#include "funcionesPlanificador.h"
+#include "libSocket.h"
+#include <commons/collections/list.h>
 
-void *selector(void* arg) {
-	fd_set master;   // conjunto maestro de descriptores de fichero
-	fd_set read_fds; // conjunto temporal de descriptores de fichero para select()
-	struct sockaddr_in addr; // dirección del servidor
-	socklen_t addrlen = sizeof(addr); // dirección del cliente
-	int fdmax;        // número máximo de descriptores de fichero
-	int listener;     // descriptor de socket a la escucha
-	int newfd;        // descriptor de socket de nueva conexión aceptada
-	char buf[256];    // buffer para datos del cliente
-	int nbytes;
-	int yes = 1;        // para setsockopt() SO_REUSEADDR, más abajo
-	int i, j;
+void eliminarCpusDesconectadas(int * socketCpu, int * numeroCpus, int maxCant){
 
-	protocolo_planificador_cpu* respuestaDeCPU = malloc(sizeof(protocolo_planificador_cpu));
+	int i;
+	t_list * auxList = list_create();
 
-
-	tParametroSelector* parametros;
-    parametros = (tParametroSelector*) arg;
-	FD_ZERO(&master);    // borra los conjuntos maestro y temporal
-	FD_ZERO(&read_fds);
-
-	struct timeval tv;
-		tv.tv_sec = 0;
-		tv.tv_usec = 100000;
-
-	listener=parametros->socket;
-
-	// añadir listener al conjunto maestro
-	FD_SET(listener, &master);
-
-	// seguir la pista del descriptor de fichero mayor
-	fdmax = listener; // por ahora es éste
-
-	// bucle principal
-	while (1) {
-		read_fds = master; // cópialo
-		if (select(fdmax + 1, &read_fds, NULL, NULL, &tv) == -1) {
-			perror("Error en el select");
-			exit(1);
-		}
-
-		// explorar conexiones existentes en busca de datos que leer
-		for (i = 0; i <= fdmax; i++) {
-			if (FD_ISSET(i, &read_fds)) { // ¡¡tenemos datos!!
-				if (i == listener) {
-					// gestionar nuevas conexiones
-					if ((newfd = accept(listener,
-							(struct sockaddr *) &addr, &addrlen)) == -1) {
-						perror("accept");
-					} else {
-						FD_SET(newfd, &master); // añadir al conjunto maestro
-						pthread_mutex_lock(&mutexListaCpus);
-						int* puntero = malloc(sizeof(int));
-						*puntero = newfd;
-						list_add(listaCpuLibres,puntero);
-						pthread_mutex_unlock(&mutexListaCpus);
-						sem_post(&hayCPU);
-						if (newfd > fdmax) {    // actualizar el máximo
-							fdmax = newfd;
-						}
-						printf("selectserver: new connection from %s on "
-								"socket %d\n", inet_ntoa(addr.sin_addr),
-								newfd);
-					}
-				} else {
-					// gestionar datos de un cliente
-					//nbytes = deserializarCPU(respuestaDeCPU,i);
-					//printf("nbytes: %d", nbytes);
-					if ((nbytes = recv(i, buf, 1, 0)) <= 0) {
-						// error o conexión cerrada por el cliente
-					//nbytes = deserializarCPU(respuestaDeCPU,i);
-					//if (nbytes <= 0){
-						if (nbytes == 0) {
-							// conexión cerrada
-							printf("selectserver: socket %d hung up\n", i);
-						} else {
-							perror("Desconexion CPU");
-						}
-						close(i); // bye!
-						FD_CLR(i, &master); // eliminar del conjunto maestro
-					}
-					else {
-						// tenemos datos de algún cliente
-						//for (j = 0; j <= fdmax; j++) {
-						//	if (FD_ISSET(j, &master)) {
-								//TODO gestionar llegada
-                            int status = 1;
-							status = deserializarCPU(respuestaDeCPU,i);
-							//if(status == 0) error_show("Desconexion de CPU");
-							 switch(respuestaDeCPU->tipoOperacion){
-
-								case 'i':{
-								}break;
-
-								case 'a':{//SI FALLA LIBERA LA CPU MOVER DE COLA EJECUTANDO A COLA DISPONIBLE
-									pthread_mutex_lock(&mutexListaCpus);
-									int* puntero = malloc(sizeof(int));
-									*puntero = i;
-									list_add(listaCpuLibres,puntero);
-									pthread_mutex_unlock(&mutexListaCpus);
-									sem_post(&hayCPU);
-								}break;
-
-								case 'f':{// LIBERAR CPU MOVER DE COLA EJECUTANDO A COLA DISPONIBLE
-									pthread_mutex_lock(&mutexListaCpus);
-									int* puntero = malloc(sizeof(int));
-									*puntero = i;
-										list_add(listaCpuLibres,puntero);
-										pthread_mutex_unlock(&mutexListaCpus);
-										sem_post(&hayCPU);
-								}break;
-							}
-							  /* pthread_mutex_lock(&mutexListaCpus);
-							   list_add(listaCpuLibres,i);
-							   pthread_mutex_unlock(&mutexListaCpus);*/
-							//}
-						//}
-					}
-				} // Esto es ¡TAN FEO!
-			}
+	for (i = 0; i < maxCant; i++){
+		if (socketCpu[i] != -1){
+			int * aux = malloc(sizeof(int));
+			*aux = socketCpu[i];
+			list_add(auxList, aux);
 		}
 	}
 
-	//return 0;
+	/*actualizo cantidad de cpu conectadas*/
+	*numeroCpus = list_size(auxList);
+
+	for (i = 0; i < *numeroCpus; i++){
+		int * aux = list_remove(auxList, 0);
+		socketCpu[i] = *aux;
+		free(aux);
+	}
+
+	list_destroy(auxList);
 }
+
+void agregarNuevaCpu(int nuevaCpu, int * socketCpu, int * numeroCpus){
+
+	/*agrego al final la nueva cpu*/
+	socketCpu[*numeroCpus - 1] = nuevaCpu;
+	int aux = *numeroCpus;
+	aux++;
+	*numeroCpus = aux;
+}
+
+int * initSocket(int cant){
+
+	int * aux = malloc(sizeof(int)*cant);
+
+	/*inicializo con -1*/
+	int i;
+	for (i = 0; i < cant; i++)
+		aux[i] = -1;
+
+	return aux;
+}
+
+void * selector(void * arg){
+
+	fd_set descriptores;
+	int numeroCpus = 0;
+	//TODO hay que setear fdmaxcant con la cantidad de cpu que se van a conectar, falta en arg
+	int fdmaxCant = 4;
+	int i;
+
+	int servidor = ((tParametroSelector*)arg)->socket;
+
+	int * socketCpu = initSocket(fdmaxCant);
+
+	while(1){
+
+		/*elimino las cpu con -1*/
+		eliminarCpusDesconectadas(socketCpu, &numeroCpus, fdmaxCant);
+
+		FD_ZERO(&descriptores);
+
+		/*añado el socket del planificador*/
+		FD_SET(servidor, &descriptores);
+		/*añado las cpus*/
+		for (i=0; i < numeroCpus; i++)
+			FD_SET (socketCpu[i], &descriptores);
+
+		/*busco el descriptor mas grande*/
+		int maxfdset = 0;
+
+		if (numeroCpus < 1){
+
+			maxfdset = socketCpu[0];
+			for (i = 0; i < fdmaxCant; i++)
+				if (socketCpu[i] > maxfdset)
+					maxfdset = socketCpu[i];
+		}
+
+		if (maxfdset < servidor)
+			maxfdset = servidor;
+
+		/*espero hasta que ocurra un evento*/
+		select (maxfdset + 1, &descriptores, NULL, NULL, NULL);
+
+		/*compruebo las cpus*/
+		for (i = 0; i < numeroCpus; i++){
+
+			if (FD_ISSET (socketCpu[i], &descriptores)){
+
+				/*leo lo que me mando la cpu*/
+				protocolo_planificador_cpu respuestaDeCPU;
+				if(deserializarCPU(&respuestaDeCPU, socketCpu[i])){
+
+					printf("llego al select.c: %c:\n", respuestaDeCPU.tipoOperacion);
+					switch(respuestaDeCPU.tipoOperacion){
+
+						case 'a':{
+								pthread_mutex_lock(&mutexListaCpus);
+								int* puntero = malloc(sizeof(int));
+								*puntero = socketCpu[i];
+								list_add(listaCpuLibres,puntero);
+								pthread_mutex_unlock(&mutexListaCpus);
+
+								sem_post(&hayCPU);
+						}
+						break;
+
+						case 'f':{
+								pthread_mutex_lock(&mutexListaCpus);
+								int* puntero = malloc(sizeof(int));
+								*puntero = socketCpu[i];
+								list_add(listaCpuLibres, puntero);
+								pthread_mutex_unlock(&mutexListaCpus);
+
+								sem_post(&hayCPU);
+						}
+						break;
+					}
+				}
+				else
+				{
+					/*lo seteo en -1 para luego eliminarlo del vector*/
+					socketCpu[i] = -1;
+					printf("CPU desconectada\n");
+				}
+			}
+		}
+
+		/*compruebo si una cpu se quiere conectar*/
+		if (FD_ISSET (servidor, &descriptores)){
+
+			int nuevaCpu;
+			/*acepto la cpu*/
+			server_acept(servidor, &nuevaCpu);
+			if (nuevaCpu != -1){
+
+				agregarNuevaCpu(nuevaCpu, socketCpu, &numeroCpus);
+
+				pthread_mutex_lock(&mutexListaCpus);
+				int * puntero = malloc(sizeof(int));
+				*puntero = nuevaCpu;
+				/*agreo la cpu a lista disponible*/
+				list_add(listaCpuLibres, puntero);
+				pthread_mutex_unlock(&mutexListaCpus);
+
+				sem_post(&hayCPU);
+
+				printf("Nueva CPU conectada\n");
+			}
+		}
+	}
+}
+
 
